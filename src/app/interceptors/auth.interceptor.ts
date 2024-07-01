db2f4d73615b50e299467324d8d324ffcb935614
claudimar <claudimar.bezerra@nairuz.com.br>
2024-07-01 11:38:03 -0300

    fix: remoção console log

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -7,7 +7,6 @@ import { environment } from "src/environments/environment";
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('interceptor')
     if(req.url.includes('brainairuz.com.br/api/') || req.url.includes(environment.URL_API) && !req.url.includes('/api/auth/')) {
       const authToken = localStorage.getItem('token');
   
