a49d293ee1aa7755fc7c507c063b0a5eb4ec1cb0
claudimar <claudimar.bezerra@nairuz.com.br>
2024-07-01 12:10:21 -0300

    fix: alteração retorno da consulta

--- backend/services/contracts/GetContractService.js
+++ backend/services/contracts/GetContractService.js
@@ -3,8 +3,12 @@ const Contracts = require("../../models/Contracts");
 module.exports.GetContractService = async ( _id ) => {
   try {
     const contract = await Contracts.findById( _id );
+
+    if(contract !== null) {
+      return contract
+    }
   
-    return contract
+    return new Error({ message: "Erro ao consultar contrato", error });
   } catch(error) {
     throw new Error({ message: "Erro ao consultar contrato", error });
   }
