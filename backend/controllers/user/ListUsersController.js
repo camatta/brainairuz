a0e348becba225c8994609a12cfe074187575447
claudimar <claudimar.bezerra@nairuz.com.br>
2024-07-02 15:57:28 -0300

    feat: alteração no retorno da consulta

--- backend/controllers/user/ListUsersController.js
+++ backend/controllers/user/ListUsersController.js
@@ -5,7 +5,7 @@ module.exports.ListUsersController = async (req, res) => {
     const listUsersService = await ListUsersService();
 
     // Retorne a lista de usuários como resposta
-    res.status(200).json({ message: listUsersService });
+    res.status(200).json({ users: listUsersService });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Ocorreu um erro ao obter os usuários.' });
