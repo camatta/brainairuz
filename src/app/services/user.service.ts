7d9a36fd1b0fa39436db738856d035d749f0e892
claudimar <claudimar.bezerra@nairuz.com.br>
2024-07-02 16:05:07 -0300

    feat: adição de tipagem

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+export type User = {
+  _id: string,
+  email: string,
+  name: string,
+  password?: string,
+  accessLevel: string,
+  team: string,
+  setor: string,
+  setorTratado: string,
+  funcao: string,
+  status: string
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,22 +23,20 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUsers(): Observable<any[]> {
-    return this.http.get<any[]>(environment.URL_API + '/api/users');
+  getUsers(): Observable<{users: User[]}> {
+    return this.http.get<{users: User[]}>(environment.URL_API + '/api/users');
   }
 
-  getUserInfo(): Observable<any> {
-    const token = localStorage.getItem('token') as any;
-    const headers = new HttpHeaders().set('Authorization', token);
-    return this.http.get<any>(environment.URL_API + '/api/users/me', { headers });
+  getUserInfo(): Observable<User> {
+    return this.http.get<User>(environment.URL_API + '/api/users/me');
   }
 
-  getUserById(userId: string): Observable<any> {
-    return this.http.get<any>(environment.URL_API + `/api/users/${userId}`);
+  getUserById(userId: string): Observable<User> {
+    return this.http.get<User>(environment.URL_API + `/api/users/${userId}`);
   }
 
-  updateUser(user: any) {
-    return this.http.put<any>(environment.URL_API + '/api/editar-usuario', user);
+  updateUser(user: User) {
+    return this.http.put<User>(environment.URL_API + '/api/editar-usuario', user);
   }
   
 }
