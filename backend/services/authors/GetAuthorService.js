f99bf868dffe3a4b53a17f8e89b64cf1f4263fea
claudimar <claudimar.bezerra@nairuz.com.br>
2024-07-11 14:10:50 -0300

    fix: remoção lançamento de erro quando autor não existe

--- backend/services/authors/GetAuthorService.js
+++ backend/services/authors/GetAuthorService.js
@@ -3,10 +3,6 @@ const Author = require('../../models/Authors');
 module.exports.GetAuthorService = async ( authorEmail ) => {
   try {
     const author = await Author.findOne({ email: authorEmail })
-
-    if(author === null) {
-      return new Error({ message: "Erro ao consultar Autor", error });
-    }
     
     return author
   } catch(error) {
